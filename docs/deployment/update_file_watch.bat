7c82ebb740856dda8b3a82e71c4082b690b746ad
Chenggang Wang <cwang@allspringglobal.com>
2024-04-17 15:38:55 -0400

    updated the batch script with correct file_watch location

--- docs/deployment/update_file_watch.bat
+++ docs/deployment/update_file_watch.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 SET SOURCE_PATH=\\Applicationfs.awsad.allspringglobal.com\FUNFI\Build_Artifacts\file_watch_0_1_14\file_watch
-SET TARGET_PATH=D:\Deployments\file_watch
+SET TARGET_PATH=D:\Apps\common\file_watch
 
 ECHO SOURCE_PATH: %SOURCE_PATH%
 ECHO TARGET_PATH: %TARGET_PATH%
